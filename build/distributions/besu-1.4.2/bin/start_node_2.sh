883de4cd5e74a5563357b4a0ca6f7a04760dcc19
Allen Liu <alex03108861@gmail.com>
2020-04-01 12:02:24 +0800

    Add script

---
+++ build/distributions/besu-1.4.2/bin/start_node_2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./besu --data-path=Clique-Network/Node-2/data --genesis-file=Clique-Network/cliqueGenesis.json --bootnodes=enode://be7c0df5d3d6dd6f6705d4d21f3d93c32dca0887aff2904d40efc0348b9e9d1832ca3832c7cf368865ce60ec65d27c786e2ddf7aee3be2865c3005f6844f6d60@127.0.0.1:30303 --network-id 123 --p2p-port=30304 --rpc-http-enabled --rpc-http-api=ETH,NET,CLIQUE --host-whitelist="*" --rpc-http-cors-origins="all" --rpc-http-port=8546
